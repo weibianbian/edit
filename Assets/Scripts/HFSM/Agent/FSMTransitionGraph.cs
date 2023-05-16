35ddac307349f399e887a1cdcce75c9bf674b9e9
CAUXIAOMAI\28941 <289417133@qq.com>
2023-05-16 23:37:24 +0800

    【update】

--- Assets/Scripts/HFSM/Agent/FSMTransitionGraph.cs
+++ Assets/Scripts/HFSM/Agent/FSMTransitionGraph.cs
@@ -1,18 +1,84 @@
 ﻿using Sirenix.OdinInspector;
+using Sirenix.Utilities;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+//或 条件
+public class OrConditionGroup : ConditionGroup
+{
+    public OrConditionGroup(FSMComponent compt) : base(compt)
+    {
+    }
 
+    public override bool Condition(TransitionBase transition)
+    {
+        for (int i = 0; i < conditions.Count; i++)
+        {
+            if (conditions[i].Check(compt))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+//与 条件
+public class AndConditionGroup : ConditionGroup
+{
+    public AndConditionGroup(FSMComponent compt) : base(compt)
+    {
+    }
+
+    public override bool Condition(TransitionBase transition)
+    {
+        for (int i = 0; i < conditions.Count; i++)
+        {
+            if (!conditions[i].Check(compt))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+public class NormalConditionGroup : ConditionGroup
+{
+    List< AndConditionGroup> andConditionGroups = new List< AndConditionGroup>();
+    List<OrConditionGroup> orConditionGroups = new List<OrConditionGroup>();
+    public NormalConditionGroup(FSMComponent compt) : base(compt)
+    {
+
+    }
+    public override bool Condition(TransitionBase transition)
+    {
+        for (int i = 0; i < andConditionGroups.Count; i++)
+        {
+            if (!andConditionGroups[i].Condition(transition))
+            {
+                return false;
+            }
+        }
+        for (int i = 0; i < orConditionGroups.Count; i++)
+        {
+            if (!orConditionGroups[i].Condition(transition))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
 public class ConditionGroup : ICondition
 {
-    List<ConditionBase> conditions = new List<ConditionBase>();
-    FSMComponent compt;
+    protected List<ConditionBase> conditions = new List<ConditionBase>();
+    protected FSMComponent compt;
     public ConditionGroup(FSMComponent compt)
     {
         this.compt = compt;
     }
-    public bool Condition(TransitionBase transition)
+    public virtual bool Condition(TransitionBase transition)
     {
         for (int i = 0; i < conditions.Count; i++)
         {
