7047e02c7c255c29753b857c5274bda6230fc291
xiaomai <xiaomai>
2023-05-17 16:26:48 +0800

    【update】

--- Assets/Scripts/EnemySpawnerGraph.cs
+++ Assets/Scripts/EnemySpawnerGraph.cs
@@ -5,22 +5,18 @@ using UnityEngine;
 
 public class EnemySpawnerGraph : SerializedMonoBehaviour
 {
-    public Agent prefab;
     public WaypointGroupGraph waypointGroupGraph;
 
     public void Awake()
     {
-        Agent agent = Instantiate(prefab);
+        //Agent agent = Instantiate(prefab);
 
-        WaypointGraph wpGraph = waypointGroupGraph.waypointGrahps[0];
-        Vector3 spawPos = wpGraph.transform.position;
+        //WaypointGraph wpGraph = waypointGroupGraph.waypointGrahps[0];
+        //Vector3 spawPos = wpGraph.transform.position;
 
-        agent.SetPos(spawPos);
-        agent.MoveTo(spawPos);
+        //agent.SetPos(spawPos);
+        //agent.MoveTo(spawPos);
 
-        FSMComponentGraph fsmComptGraph= wpGraph.GetComponent<FSMComponentGraph>();
-
-        fsmComptGraph.GenerateFSM(agent);
     }
 
 }
