bb5cc60ec96d04143bb6c4abd127242fbe794352
xiaomai <xiaomai>
2023-07-18 20:42:46 +0800

    bt

--- Assets/Scripts/Game/Actor.cs
+++ Assets/Scripts/Game/Actor.cs
@@ -11,6 +11,7 @@ namespace RailShootGame
     {
         void UpdateLogic(float delta);
     }
+    //Spawn -----  Init  ----Activate
     public class Actor
     {
         public ActorObject actorObject;
@@ -33,9 +34,15 @@ namespace RailShootGame
             hsm = new HierarchicalStateMachineCompt(this);
             hsm.Init();
         }
+        //创建的时候，需要填充配置信息
         public void Spawn()
         {
             move.StopMove(EMoveStatus.MOVE_STATUS_DONE);
+        }
+        //通知脚本怪物已被触发器或手电筒激活
+        public void Activate()
+        {
+
         }
         public Vector3 GetOrigin()
         {
