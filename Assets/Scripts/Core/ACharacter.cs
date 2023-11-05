18f92ed7dec0d433cfbd5e18edd60628b8ff691c
CAUXIAOMAI\28941 <289417133@qq.com>
2023-11-05 23:38:04 +0800

    路径

--- Assets/Scripts/Core/ACharacter.cs
+++ Assets/Scripts/Core/ACharacter.cs
@@ -33,12 +33,13 @@ namespace RailShootGame
         }
     }
 
-    public class Character : Pawn
+    public class ACharacter : Pawn
     {
         public bool bPressedJump = false;
         public float JumpKeyHoldTime = 0;
         public bool bWasJumping = false;
         private int JumpCurrentCount = 0;
+        private int JumpCurrentCountPreJump = 0;
         private float JumpForceTimeRemaining = 0;
 
         public CharacterMovementComponent CharacterMovement;
@@ -94,6 +95,35 @@ namespace RailShootGame
         private float GetJumpMaxHoldTime()
         {
             return JumpKeyHoldTime;
+        }
+        public void ResetJumpState()
+        {
+            bPressedJump = false;
+            bWasJumping = false;
+            JumpKeyHoldTime = 0.0f;
+            JumpForceTimeRemaining = 0.0f;
+
+            if (CharacterMovement != null && !CharacterMovement.IsFalling())
+            {
+                JumpCurrentCount = 0;
+                JumpCurrentCountPreJump = 0;
+            }
+        }
+        public void OnMovementModeChanged(EMovementMode PrevMovementMode, int PrevCustomMode)
+        {
+            if (!bPressedJump || !CharacterMovement.IsFalling())
+            {
+                ResetJumpState();
+            }
+
+            // Record jump force start time for proxies. Allows us to expire the jump even if not continually ticking down a timer.
+            //if (bProxyIsJumpForceApplied && CharacterMovement->IsFalling())
+            //{
+            //    ProxyJumpForceStartedTime = GetWorld()->GetTimeSeconds();
+            //}
+
+            //K2_OnMovementModeChanged(PrevMovementMode, CharacterMovement->MovementMode, PrevCustomMode, CharacterMovement->CustomMovementMode);
+            //MovementModeChangedDelegate.Broadcast(this, PrevMovementMode, PrevCustomMode);
         }
     }
 }
--- Assets/Scripts/Game/ActorCompts/CharacterMovementComponent.cs
+++ Assets/Scripts/Game/ActorCompts/CharacterMovementComponent.cs
@@ -1,13 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 namespace RailShootGame
 {
     public class CharacterMovementComponent : NavMovementComponent
     {
         public EMovementMode MovementMode;
-        protected Character CharacterOwner;
+        public EMovementMode GroundMovementMode;
+        protected ACharacter CharacterOwner;
         public float JumpZVelocity;
-
+        public int CustomMovementMode;
         public bool IsFalling()
         {
             return MovementMode == EMovementMode.MOVE_Falling;
@@ -21,18 +23,49 @@ namespace RailShootGame
             }
             return false;
         }
-        public void SetMovementMode(EMovementMode NewMovementMode)
+        public virtual void SetMovementMode(EMovementMode NewMovementMode, int NewCustomMode = 0)
         {
+            if (NewMovementMode != EMovementMode.MOVE_Custom)
+            {
+                NewCustomMode = 0;
+            }
             if (MovementMode == NewMovementMode)
             {
-                return;
+                if ((NewMovementMode != EMovementMode.MOVE_Custom) || NewCustomMode == CustomMovementMode)
+                {
+                    return;
+                }
             }
             EMovementMode PrevMovementMode = MovementMode;
+            int PrevCustomMode = CustomMovementMode;
             MovementMode = NewMovementMode;
+            CustomMovementMode = NewCustomMode;
+
+        }
+        public virtual void OnMovementModeChanged(EMovementMode PreviousMovementMode, int PreviousCustomMode)
+        {
+            if (MovementMode == EMovementMode.MOVE_Walking)
+            {
+                Velocity.y = 0;
+                GroundMovementMode = MovementMode;
+
+            }
+            else
+            {
+                if (MovementMode == EMovementMode.MOVE_None)
+                {
+                    CharacterOwner.ResetJumpState();
+                }
+            }
+            if (MovementMode == EMovementMode.MOVE_Falling && PreviousMovementMode != EMovementMode.MOVE_Falling)
+            {
+
+            }
+            CharacterOwner.OnMovementModeChanged(PreviousMovementMode, PreviousCustomMode);
         }
         public override void TickComponent(float DeltaTime)
         {
-            ControlledCharacterMove(Vector3.zero,1);
+            ControlledCharacterMove(Vector3.zero, 1);
         }
         public void ControlledCharacterMove(Vector3 InputVector, float DeltaSeconds)
         {
